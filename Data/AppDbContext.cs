cb62f38c5cd9adeeaefcf12725253d1919c44d28
Miraliba Jadeja <mjadeja482@rku.ac.in>
2025-03-02 10:37:54 +0530

    Migrations Added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAdminPortal.Models;
+
+namespace UserAdminPortal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
