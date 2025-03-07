6dc02ffbd25c682354f5e5ff2a25a50bc0146010
Artikumari-2105 <universalaarti@gmail.com>
2025-03-07 23:17:09 +0530

    ViewModels Added

---
+++ ViewModel/ChangePasswordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAdminPortal.ViewModel
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is Required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Compare("ConfirmNewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmNewPassword { get; set; }
+        
+    }
+}
---
+++ ViewModel/VerifyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAdminPortal.ViewModel
+{
+    public class VerifyViewModel
+    {
+        [Required(ErrorMessage ="Email is Required")]
+        [EmailAddress]
+        public string Email { get; set; }
+       
+    }
+}
