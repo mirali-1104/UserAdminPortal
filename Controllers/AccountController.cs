e70a1bd2147ef91c5ce41cd80dc91760dadd4668
Miraliba Jadeja <mjadeja482@rku.ac.in>
2025-03-08 12:21:05 +0530

    Register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using UserAdminPortal.Models;
+using UserAdminPortal.ViewModel;
 
 namespace UserAdminPortal.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager; 
+        }
         public IActionResult Login()
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User users = new User
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
     }
 }
