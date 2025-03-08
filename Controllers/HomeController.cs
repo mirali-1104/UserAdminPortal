d8f5a7f3a7e980bd1521deef33affd4590285874
Artikumari-2105 <universalaarti@gmail.com>
2025-03-09 02:23:04 +0530

    UI added successfully

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace UserAdminPortal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult ContactUs()
+        {
+            return View(); 
+        }
     }
 }
